0daee17be8d2e348ade6a74259b12ae3fcdef5f5
Ravi Shankar <wafflespeanut@gmail.com>
2016-11-01 13:37:48 +0530

    Add option for marking traced paths

--- src/trie.rs
+++ src/trie.rs
@@ -4,8 +4,14 @@ use std::hash::Hash;
 
 #[derive(Debug)]
 pub struct Trie<T: Eq + Hash, S> {
+    // Since we're using HashMap, it's better if we have the depth
+    // as minimum as possible to avoid clutter.
     node: HashMap<T, Trie<T, S>>,
     value: Option<S>,
+    // Marker type to remember overwritten values. This is useful when
+    // we're interested in unique values. Getting and replacing values is
+    // probably a bad idea, since it takes O(n) time.
+    pub is_traced_path: bool,
 }
 
 impl<T: Eq + Hash, S> Trie<T, S> {
@@ -13,6 +19,7 @@ impl<T: Eq + Hash, S> Trie<T, S> {
         Trie {
             value: None,
             node: HashMap::new(),
+            is_traced_path: false,
         }
     }
 
@@ -22,22 +29,30 @@ impl<T: Eq + Hash, S> Trie<T, S> {
                 let mut entry = (&mut self.node).entry(thing).or_insert(Trie::new());
                 entry.insert(iterator, value);
             },
-            None => {
-                self.value = Some(value);
+            None => {   // End of iteration: Mark if there's a value already
+                if self.value.is_some() && self.node.is_empty() {
+                    self.is_traced_path = true;
+                } else {
+                    self.value = Some(value);
+                }
             },
         }
     }
 
-    pub fn get<I: Iterator<Item = T>>(&self, iterator: I) -> Option<&S> {
+    pub fn get<I: Iterator<Item = T>>(&self, iterator: I, check_unique: bool) -> Option<&S> {
         let mut current_node = self;
         for thing in iterator {
-            if let Some(map) = current_node.node.get(&thing) {
-                current_node = &map;
+            if let Some(trie) = current_node.node.get(&thing) {
+                current_node = &trie;
             } else {
                 return None
             }
         }
 
-        current_node.value.as_ref()
+        if check_unique && current_node.is_traced_path {
+            None
+        } else {
+            current_node.value.as_ref()
+        }
     }
 }
