ffe1bb662560b03fa693b0028525535e3d5e107e
Ravi Shankar <wafflespeanut@gmail.com>
2016-11-19 23:48:41 +0530

    Cache the occurrence indices when building FM index

--- src/bwt.rs
+++ src/bwt.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 // FIXME: Try the efficient algorithm (takes only O(n) time with additional O(n) space)
 // https://de.wikipedia.org/wiki/Suffix-Array-Induced-Sorting
-pub fn bwt(mut input: Vec<u8>) -> Vec<u8> {
+pub fn bwt<F: FnMut(u8)>(mut input: Vec<u8>, mut f: F) -> Vec<u8> {
     input.push(0);
 
     // get the sorted suffix array
@@ -17,19 +17,14 @@ pub fn bwt(mut input: Vec<u8>) -> Vec<u8> {
         } else {
             rots[i] = input[input.len() - rotations[i].len() - 1];
         }
+
+        f(rots[i]);     // call the function with the final value
     }
 
     rots
 }
 
-pub fn occurrence_index(input: &[u8]) -> HashMap<u8, usize> {
-    // get the byte distribution
-    let mut map = HashMap::new();
-    for i in input {
-        let mut count = map.entry(*i).or_insert(0);
-        *count += 1;
-    }
-
+pub fn occurrence_index(map: HashMap<u8, usize>) -> HashMap<u8, usize> {
     // sort the bytes and locate the index of their first occurrences
     let mut chars = map.keys().collect::<Vec<_>>();
     chars.sort();
@@ -45,7 +40,14 @@ pub fn occurrence_index(input: &[u8]) -> HashMap<u8, usize> {
 }
 
 pub fn ibwt(input: Vec<u8>) -> Vec<u8> {
-    let mut occ_map = occurrence_index(&input);
+    // get the byte distribution
+    let mut map = HashMap::new();
+    for i in &input {
+        let mut count = map.entry(*i).or_insert(0);
+        *count += 1;
+    }
+
+    let mut occ_map = occurrence_index(map);
 
     // generate the LF vector
     let mut lf = vec![0; input.len()];
@@ -69,46 +71,60 @@ pub fn ibwt(input: Vec<u8>) -> Vec<u8> {
 
 pub struct FMIndex {
     data: Vec<u8>,
+    count_cache: Vec<usize>,    // remember the occurrence of each char (caching)
     occ_map: HashMap<u8, usize>,
 }
 
 impl FMIndex {
     pub fn new(data: Vec<u8>) -> FMIndex {
-        let bwt_data = bwt(data);
+        let mut idx = 0;
+        let mut map = HashMap::new();
+        let mut count = vec![0; data.len() + 1];
+        let bwt_data = bwt(data, |i| {
+            let mut c = map.entry(i).or_insert(0);
+            *c += 1;
+            count[idx] = *c;
+            idx += 1;
+        });
+
         FMIndex {
-            occ_map: occurrence_index(&bwt_data),
+            occ_map: occurrence_index(map),
+            count_cache: count,
             data: bwt_data,
         }
     }
 
-    fn nearest_lf(&self, idx: usize, ch: u8) -> usize {
+    fn nearest(&self, idx: usize, ch: u8) -> usize {
         match self.occ_map.get(&ch) {
-            Some(occ) => occ + self.data[0..idx].iter().filter(|&i| *i == ch).count(),
+            Some(occ) => {
+                occ + (0..idx).rev()
+                              .find(|&i| self.data[i] == ch)
+                              .map(|i| self.count_cache[i])
+                              .unwrap_or(0)
+            },
             None => 0,
         }
     }
 
     // FIXME: We need "checkpointed" index!
-    pub fn search(&mut self, query: &str) -> Vec<usize> {
+    pub fn search(&mut self, query: &str) -> Option<usize> {
         let (mut top, mut bottom) = (0, self.data.len());
         for ch in query.as_bytes().iter().rev() {
-            top = self.nearest_lf(top, *ch);
-            bottom = self.nearest_lf(bottom, *ch);
+            top = self.nearest(top, *ch);
+            bottom = self.nearest(bottom, *ch);
         }
-
-        let mut results = vec![];
 
         for idx in top..bottom {
             let mut pos = 0;
             let mut i = idx;
             while self.data[i] != 0 {
                 pos += 1;
-                i = self.nearest_lf(i, self.data[i]);
+                i = self.nearest(i, self.data[i]);
             }
 
-            results.push(pos);
+            return Some(pos)    // break on first find
         }
 
-        return results
+        None
     }
 }
