425a3f9b0c1675d694d407585bdf35c48364b944
Ravi Shankar <wafflespeanut@gmail.com>
2016-12-01 19:14:44 +0530

    Working get and push

--- src/rust-fillings/src/lib.rs
+++ src/rust-fillings/src/lib.rs
@@ -1,7 +1,7 @@
 use std::usize;
 
 #[derive(Eq, PartialEq, Clone, Hash)]
-pub struct BitVec {
+pub struct BitsVec {
     inner: Vec<usize>,
     units: usize,
     bits: usize,
@@ -9,15 +9,15 @@ pub struct BitVec {
     leftover: usize,
 }
 
-impl BitVec {
-    pub fn new(bits: usize) -> BitVec {
+impl BitsVec {
+    pub fn new(bits: usize) -> BitsVec {
         let max = usize::MAX.count_ones() as usize;
         if bits > max {
             // We can store more bits, but then we might need BigInt to get them out!
             panic!("cannot hold more than {} bits at a time", max);
         }
 
-        BitVec {
+        BitsVec {
             inner: vec![0],
             units: 0,
             bits: bits,
@@ -27,9 +27,8 @@ impl BitVec {
     }
 
     pub fn push(&mut self, mut value: usize) {
-        if value >> self.bits != 0 {
-            panic!("size of {} is more than allowed bits ({})", value, self.bits)
-        }
+        assert!(value >> self.bits == 0,
+                "input size is more than allowed size ({} >= {})", value, 2usize.pow(self.bits as u32));
 
         let mut idx = self.inner.len() - 1;
         let shift_amount;
@@ -38,7 +37,7 @@ impl BitVec {
             let left = self.bits - self.leftover;
             self.inner[idx] |= value >> left;
             if self.leftover != 0 {     // special case, in which masking would result in zero!
-                value &= 1 << (left - 1);
+                value &= (1 << left) - 1;
             }
 
             self.inner.push(0);
@@ -53,6 +52,27 @@ impl BitVec {
         value <<= shift_amount;
         self.inner[idx] |= value;
         self.units += 1;
+    }
+
+    pub fn get(&self, i: usize) -> Option<usize> {
+        if i >= self.units {
+            return None
+        }
+
+        let idx = i * self.bits / self.max_bits;
+        let bits = (i * self.bits) % self.max_bits;
+        let mut val = self.inner[idx];
+        if bits != 0 {
+            val &= (1 << (self.max_bits - bits)) - 1;
+        }
+
+        let diff = self.max_bits - bits;
+        if diff >= self.bits {
+            Some(val >> (diff - self.bits))
+        } else {
+            let shift = self.bits - diff;
+            Some((val << shift) | (self.inner[idx + 1] >> (self.max_bits - shift)))
+        }
     }
 
     pub fn len(&self) -> usize {
