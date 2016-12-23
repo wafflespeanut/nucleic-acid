7a326505b7aa40a6100c3e898a9f422a58abcb81
Ravi Shankar <wafflespeanut@gmail.com>
2016-12-23 18:46:51 +0530

    Benchmarking SA and FM index

---
+++ benches/bwt.rs
@@ -0,0 +1,49 @@
+#![feature(test)]
+
+extern crate helix;
+#[macro_use]
+extern crate lazy_static;
+extern crate test;
+extern crate rand;
+
+use helix::{suffix_array, FMIndex};
+use rand::Rng;
+use test::Bencher;
+
+lazy_static! {
+    static ref DATA: Vec<u8> = {
+        let mut rng = rand::thread_rng();
+        let bases = vec![65, 67, 71, 84];
+        (0..10000).map(|_| bases[rng.gen_range(0, bases.len())]).collect()
+    };
+}
+
+#[bench]
+fn bench_sort_rotations_10000_random_values(b: &mut Bencher) {
+    b.iter(|| {
+        let mut rotations = (0..DATA.len()).map(|i| &DATA[i..]).collect::<Vec<_>>();
+        rotations.sort();
+    })
+}
+
+#[bench]
+fn bench_suffix_array_10000_random_values(b: &mut Bencher) {
+    b.iter(|| {
+        suffix_array(&DATA);
+    })
+}
+
+#[bench]
+fn bench_fm_index_10000_random_values_constructor(b: &mut Bencher) {
+    b.iter(|| {
+        FMIndex::new(DATA.clone());
+    })
+}
+
+#[bench]
+fn bench_fm_index_10000_random_values_getter(b: &mut Bencher) {
+    let index = FMIndex::new(DATA.clone());
+    b.iter(|| {
+        index.search("AAA");
+    })
+}
